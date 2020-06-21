7a798e54db6e3a4ca386cf37374ebf64d2a01972
liuyunxiang <liuyunxng@foxmail.com>
2020-06-21 09:34:15 +0800

    add methods: decreaseQuality, handleSellDayPassed,

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -33,14 +33,36 @@ class Shop {
     }
   }
 
-  updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+  decreaseQuality (i) {
+    if (this.items[i].quality > 0) {
+      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+        this.items[i].quality = this.items[i].quality - 1;
+      }
+    }
+  }
+
+  handleSellDayPassed (i) {
+    if (this.items[i].name != 'Aged Brie') {
+      if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].quality > 0) {
           if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
             this.items[i].quality = this.items[i].quality - 1;
           }
         }
+      } else {
+        this.items[i].quality = this.items[i].quality - this.items[i].quality;
+      }
+    } else {
+      if (this.items[i].quality < 50) {
+        this.items[i].quality = this.items[i].quality + 1;
+      }
+    }
+  }
+
+  updateQuality() {
+    for (let i = 0; i < this.items.length; i++) {
+      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+        this.decreaseQuality(i)
       } else {
         if (this.items[i].quality < 50) {
           this.increaseQuality(i)
@@ -50,21 +72,7 @@ class Shop {
       this.updateSellIn(i)
 
       if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
-        }
+        this.handleSellDayPassed(i)
       }
     }
 
