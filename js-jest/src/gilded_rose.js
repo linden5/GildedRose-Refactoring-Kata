aa639a0a0809e0ddab4448a45d9517d7064b05ff
liuyunxiang <liuyunxng@foxmail.com>
2020-06-21 10:36:45 +0800

    eliminate distributed quality condition statement

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -25,28 +25,20 @@ class Shop {
   }
 
   increaseQuality (item) {
-    if (item.quality < 50) {
-      item.quality = item.quality + 1;
-      if (item.name == ITEM_NAME.BACKSTAGE_PASSES) {
-        if (item.sellIn < 11) {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
-          }
-        }
-        if (item.sellIn < 6) {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
-          }
-        }
+    this.updateQualityValue(item, 1);
+    if (item.name == ITEM_NAME.BACKSTAGE_PASSES) {
+      if (item.sellIn < 11) {
+        this.updateQualityValue(item, 1);
+      }
+      if (item.sellIn < 6) {
+        this.updateQualityValue(item, 1);
       }
     }
   }
 
   decreaseQuality (item) {
-    if (item.quality > 0) {
-      if (item.name != ITEM_NAME.SULFURAS) {
-        item.quality = item.quality - 1;
-      }
+    if (item.name != ITEM_NAME.SULFURAS) {
+      this.updateQualityValue(item, -1);
     }
   }
 
@@ -54,38 +46,30 @@ class Shop {
     if (item.sellIn < 0) {
       if (item.name != ITEM_NAME.AGED_BRIE) {
         if (item.name != ITEM_NAME.BACKSTAGE_PASSES) {
-          if (item.quality > 0) {
-            if (item.name != ITEM_NAME.SULFURAS) {
-              item.quality = item.quality - 1;
-            }
+          if (item.name != ITEM_NAME.SULFURAS) {
+            this.updateQualityValue(item, -1);
           }
         } else {
-          item.quality = item.quality - item.quality;
+          this.updateQualityValue(item, -item.quality);
         }
       } else {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1;
-        }
+        this.updateQualityValue(item, 1);
       }
     }
   }
 
-  // updateQualityValue (item, value) {
-  //   if (value !== 0) {
-  //     const newValue = item.quality + value;
-  //     if (newValue > 50) {
-  //       newValue = 50;
-  //     } else if (newValue < 0) {
-  //       newValue = 0;
-  //     }
+  updateQualityValue (item, value) {
+    if (value !== 0) {
+      let newValue = item.quality + value;
+      if (newValue > 50) {
+        newValue = 50;
+      } else if (newValue < 0) {
+        newValue = 0;
+      }
 
-  //     item.quality = newValue;
-  //   }
-  // }
-
-  // updateQualityOfSulfuras () {
-
-  // }
+      item.quality = newValue;
+    }
+  }
 
   updateQualityByName (item) {
     if (item.name != ITEM_NAME.AGED_BRIE && item.name != ITEM_NAME.BACKSTAGE_PASSES) {
